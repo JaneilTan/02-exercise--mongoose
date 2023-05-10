7542f20579ff108acbe5348b9f7f3390874e7efd
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-10 20:16:53 +1200

    Completed mongoose queries

--- src/queries.js
+++ src/queries.js
@@ -1,14 +1,24 @@
-// Return all items from the database
-const getAllItems = async () => {};
+const Inventory = require("./InventoryModel");
 
+
+// Return all items from the database
+const getAllItems = async () => {
+   return await Inventory.find({});
+  };
 // Return an item that matches the id
-const getItemById = async (id) => {};
+const getItemById = async (id) => {
+  return await Inventory.findById(id);
+};
 
 // Return items with a status "D"
-const getDamagedItems = async () => {};
+const getDamagedItems = async () => {
+  return await Inventory.find({ status: "D"});
+};
 
 // Return items with a status "A" and qty less than 30
-const getLowStockItems = async () => {};
+const getLowStockItems = async () => {
+  return await Inventory.find({ status: "A", qty: { $lte:30} });
+};
 
 module.exports = {
   getAllItems,
